2462c03daa472d28fd4de2a008cb0eef4309a120
kbritt510 <kbritt510@gmail.com>
2020-11-15 14:46:22 -0600

    added some functionality to BoardUnitsController

--- team-builder-tactics-api/app/controllers/board_units_controller.rb
+++ team-builder-tactics-api/app/controllers/board_units_controller.rb
@@ -5,4 +5,18 @@ class BoardUnitsController < ApplicationController
         render json: board_units
     end
 
+    def create
+        board_unit = BoardUnit.create(board_unit_params)
+        board = Board.find(board_unit_params[:board_id])
+        render json: board
+    end
+
+
+
+    private
+
+    def board_unit_params
+        params.require(:board_unit).permit(:unit_id, :board_it, :hex)
+    end
+
 end
