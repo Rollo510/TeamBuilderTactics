753a3c9c035bb0c51578d0b6b536c916f6b49789
kbritt510 <kbritt510@gmail.com>
2020-11-18 15:03:14 -0600

    added creating BoardUnits

--- team-builder-tactics-api/app/controllers/board_units_controller.rb
+++ team-builder-tactics-api/app/controllers/board_units_controller.rb
@@ -2,21 +2,29 @@ class BoardUnitsController < ApplicationController
 
     def index
         board_units = BoardUnit.all
-        render json: board_units
+        render json: board_unit_index
     end
 
     def create
-        board_unit = BoardUnit.create(board_unit_params)
-        board = Board.find(board_unit_params[:board_id])
-        render json: board
+        counter = 0
+        board = Board.find_or_create_by(name: params[:name])
+        params[:elements].each do |e|
+            id = e.split('_')[1]
+            unit = Unit.find_by(id: id)
+            // params[:hex][counter].to_i
+            x = BoardUnit.create(board_id: board.id, unit_id: unit.id, hex: params[:hex][counter] //may need to split)
+            counter++
+        end
+        binding.pry
+        render json: board_units
     end
 
 
 
     private
 
-    def board_unit_params
-        params.require(:board_unit).permit(:unit_id, :board_it, :hex)
-    end
+    # def board_unit_params
+    #     params.require(:board_units).permit(:name, :elements)
+    # end
 
 end
